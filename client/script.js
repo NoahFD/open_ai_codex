27bb69ceba2fd0de811513d6397eb4a53f94c6c9
NoahFD <weipingtong128@gmail.com>
2024-06-06 13:07:51 +0800

    update server

--- client/script.js
+++ client/script.js
@@ -37,3 +37,66 @@ function generateUniqueId() {
 
   return `id-${timestamp}-${hexaDecimalsString}`;
 }
+
+function chatStripe(isAi, value, uniqueId) {
+  return `
+      <div class="wrapper ${isAi && "ai"}">
+      <div class="chat">
+      <div class="profile">
+      <img src="${isAi ? bot : user}" alt="${isAi ? "bot" : "user"}"/>
+      
+      </div>
+      <div class="message" id=${uniqueId}>${value}</div>
+      </div></div>
+      `;
+}
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  //user's chat stripe
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
+
+  form.reset();
+
+  //bot's chat stripe
+  const uniqueId = generateUniqueId();
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+
+  chatContainer.scrollTop = chatContainer.scrollHeight; // put new message in view
+
+  const messageDiv = document.getElementById(uniqueId);
+
+  loader(messageDiv);
+
+  const response = await fetch("http://localhost:5001", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
+
+  clearInterval(loadInterval);
+  messageDiv.innerHTML = "";
+
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = await response.text();
+
+    messageDiv.innerHTML = "Something went wrong";
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (e) => {
+  if (e.keyCode === 13) {
+    handleSubmit(e);
+  }
+});
